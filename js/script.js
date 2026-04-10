dcdb24dd4d1b613e6132b271a02b415ec2f3201a
eduardoOleta <oletaeduardo6@gmail.com>
2026-04-10 15:56:41 -0700

    Mejor estructurado y con tercera oagina vacia

--- js/script.js
+++ js/script.js
@@ -3,12 +3,42 @@ let submitBtn = document.getElementById("submitBtn");
 let cuadrocont = document.getElementById("cuadrocont");
 let usernamebuscar = document.getElementById("usernamebuscar");
 let buscarBtn = document.getElementById("buscarBtn");
+let verBtn =document.getElementById("VerBtn");
 
 var usuarios = [];
 
 submitBtn.addEventListener("click", () => enviarDatos());
+verBtn.addEventListener("click", () => ordenar());
 buscarBtn.addEventListener("click", () => buscarUsuario());
 
+function ordenar(){
+    for(let usuarios=0; usuarios=!"";usuarios++){
+        let card = document.createElement("div");
+        card.classList.add("card");
+
+        let img = document.createElement("img");
+        img.src = `https://github.com/${nombre}.png`;
+
+        let p = document.createElement("p");
+        p.textContent = nombre;
+
+
+        let btnCancelar = document.createElement("button");
+        btnCancelar.textContent = "Cancelar";
+        btnCancelar.classList.add("btn2");
+        btnCancelar.addEventListener("click", () => {
+            card.remove();
+        });
+
+        card.appendChild(img);
+        card.appendChild(p);
+        card.appendChild(btnAceptar);
+        card.appendChild(btnCancelar);
+
+        cuadrocont.appendChild(card);
+
+    }
+}
 
 function enviarDatos() {
     let nombre = username.value.trim();
@@ -30,6 +60,7 @@ function enviarDatos() {
     btnAceptar.addEventListener("click", () => {
         usuarios.push(nombre);
         card.remove();
+        location.href = 'verPerfil.html';
     });
 
     let btnCancelar = document.createElement("button");
@@ -56,48 +87,6 @@ function enviarDatos() {
 
 
 
-function enviarDatos22() {
-    let nombre = username.value.trim();
-
-    if (nombre === "") return;
-
-    // Contenedor principal
-    let card = document.createElement("div");
-    card.classList.add("card");
-
-    // Imagen
-    let img = document.createElement("img");
-    img.src = `https://github.com/${nombre}.png`;
-
-    // Nombre
-    let p = document.createElement("p");
-    p.textContent = nombre;
-
-    // Botón Aceptar
-    let btnAceptar = document.createElement("button");
-    btnAceptar.textContent = "Aceptar";
-    btnAceptar.classList.add("btn1");
-    btnAceptar.addEventListener("click", () => {
-        usuarios.push(nombre);
-        card.remove();
-    });
-
-
-    // Botón Cancelar
-    let btnCancelar = document.createElement("button");
-    btnCancelar.textContent = "Cancelar";
-    btnCancelar.classList.add("btn2");
-    btnCancelar.addEventListener("click", () => {
-        card.remove();
-    });
-
-    // Armar la card
-    card.appendChild(img);
-    card.appendChild(p);
-    card.appendChild(btnAceptar);
-    card.appendChild(btnCancelar);
-
-    cuadrocont.appendChild(card);
-
-    username.value = "";
+function buscarUsuario() {
+   
 }
